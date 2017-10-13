318a1eaa78103bdcd0663c4f80b7f9305b6a3ac5
Joseph-Melberg <jbmelberg@eiu.edu>
2017-10-12 21:27:03 -0500

    Breakpoint lines on scrollbar

--- ScrollBarHandler.cpp
+++ ScrollBarHandler.cpp
@@ -1,11 +1,7 @@
 #include "ScrollBarHandler.h"
 #include "computer.h"
 #include "QLinkedList"
-ScrollBarHandler::ScrollBarHandler(QObject *parent) : QObject(parent)
-{
-//    bars = new QLinkedList<H/>();
-
-}
+ScrollBarHandler::ScrollBarHandler(QObject *parent) : QObject(parent){}
 void ScrollBarHandler::addScrollBar(HighlightScrollBar* bar)
 {
     bars.append(bar);
@@ -22,6 +18,18 @@ void ScrollBarHandler::update()
         for(int i = 0;i<= 9;i++)
         {
             bar->addHighlight(Highlight(Highlight::regpoint,regvals[i],i,Highlight::NormalPriority));
+        }
+    }
+    for(mem_addr_t addr = 0; addr < 0xFFFF;addr++)
+    {
+        //update registers
+        breakpoint_t breaker = Computer::getDefault()->getMemBreakPoint(addr);
+        if(breaker!=nullptr)
+        {
+            foreach(HighlightScrollBar* bar, bars)
+            {
+                bar->addHighlight(Highlight(Highlight::breakpoint,addr,2,Highlight::NormalPriority));
+            }
         }
     }
 }
